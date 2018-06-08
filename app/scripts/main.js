bb297ea2d68abe0fd151b4b5728e5493515a8c49
Ivan Zlatev <izlateff@gmail.com>
2018-06-07 19:45:33 -0500

    Add Google Maps and dummy nav; change to Bootstrap 3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,25 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("optionsBox").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("optionsBox").style.width = "0";
+    document.getElementById("map").style.marginLeft = "0";
+}
+
+var map;
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7413549, lng: -73.9980244},
+      zoom: 13,
+      mapTypeControl: false
+    });
+}
+
 // Here's my data model
 var ViewModel = function(first, last) {
     this.firstName = ko.observable(first);
